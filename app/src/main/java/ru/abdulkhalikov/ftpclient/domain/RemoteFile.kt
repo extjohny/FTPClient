6293838256f406e3c2f382ad5498e2a0ba205322
johny <verifykaq@gmail.com>
2026-01-17 17:55:23 +0300

    Pojo RemoteFile.kt created

---
+++ app/src/main/java/ru/abdulkhalikov/ftpclient/domain/RemoteFile.kt
@@ -0,0 +1,30 @@
+package ru.abdulkhalikov.ftpclient.domain
+
+import org.apache.commons.net.ftp.FTPFile
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+data class RemoteFile(
+    val name: String,
+    val type: Int,
+    val size: Long,
+    val timestamp: Calendar?,
+    val permissions: String?,
+    val path: String
+) {
+    val isDirectory: Boolean
+        get() = type == FTPFile.DIRECTORY_TYPE
+
+    val formattedSize: String
+        get() = when {
+            size < 1024 -> "$size B"
+            size < 1024 * 1024 -> "${size / 1024} KB"
+            else -> "${size / (1024 * 1024)} MB"
+        }
+
+    val formattedDate: String?
+        get() = timestamp?.let {
+            SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.getDefault()).format(it.time)
+        }
+}
